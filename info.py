1b68e08eb85e9fec20d6a9bc8aa6688d1f24895c
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2026-09-16 21:09:46 +0530

    Restore credit comments and update image URL

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7744617382:AAEGHj8g9fBTEMIqFoN2coHS78523uD
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://envs.sh/IZQ.png')).split()
+PICS = (environ.get('PICS', 'https://i.picrd.com/images/tjiGnhDDMJZ.png')).split()
 
 
 # Admins & Users
